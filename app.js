26a9da860737fadeb8dc2a5191f8089b0d274f70
Joshua Blanchard <joshua.blanchard@ketch.com>
2023-03-23 17:33:30 -0700

    added base of app

---
+++ app.js
@@ -0,0 +1,25 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+const { Client } = require('pg')
+const client = new Client()
+
+client.connect()
+
+client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
+    console.log(err ? err.stack : res.rows[0].message) // Hello World!
+    client.end()
+})
+
