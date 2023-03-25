eda15d1ba4392c475df5ed0237f4e4e7f04ee0c2
Joshua Blanchard <joshua.blanchard@ketch.com>
2023-03-25 16:54:11 -0700

    finished basic site visit logging

--- main.js
+++ main.js
@@ -2,16 +2,15 @@ const storeRequestInfo = require('./requestInfoCollection.js')
 
 function requestListener(request, response) {
     storeRequestInfo(request)
-    response.end('Received')
+    response.end('Welcome')
     console.log('got request')
 }
 
 const http = require('http');
 const server = http.createServer(requestListener);
 
-const hostname = 'localhost';
 const port = 3000;
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+    console.log(`Server running`);
 });
--- requestInfoCollection.js
+++ requestInfoCollection.js
@@ -6,10 +6,22 @@ function getCurrentUTCDate() {
 
 const Db = require('./db/index')
 const RequestIp = require('@supercharge/request-ip')
+
 module.exports =  function storeRequestInformation(request) {
     const clientIp = RequestIp.getClientIp(request)
     const receivedAt = getCurrentUTCDate()
 
-    Db.query()  // finish later
-}
+    const insertVisitQuery =
+        `INSERT INTO visits (client_ip_address, time_of_visit) 
+        VALUES (\'${clientIp}\', \'${receivedAt}\');`
 
+    function queryCallback(err, result) {
+        if (err) {
+            console.error('Error executing query', err.stack)
+            return
+        }
+        console.log('insert visit query was successful')
+    }
+
+    Db.query(insertVisitQuery, [], queryCallback)
+}
